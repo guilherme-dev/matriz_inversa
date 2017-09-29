93f65889ffe251aa692fb042a747443662950ca9
Guilherme Santos <ggs12@inf.ufpr.br>
2017-09-28 23:32:39 -0300

    testes de indices

--- testes.c
+++ testes.c
@@ -1,21 +1,34 @@
-#include <stdio.h>		
+#include <stdio.h>
 #include <stdlib.h>
-
+#define LIN (i*n) - k
 int main(int argc, char const *argv[])
 {
-	int n = 100;
-	int size = n * n + n / 2;
+	int n = 4;
+	int size = ((n * n) + n) / 2;
 	printf("size: %d\n", size);
 	int v[size];
 
-	int i, j;
+	int i, j, k, col;
 
 	for (i = 0; i < size; ++i)
 	{
 		v[i] = i;
 	}
 
-	
+    k = 0;
+    col = n-1;
+	for (i = 0; i < n; ++i) {
+        printf("%d ", v[i]);
+    }
+    printf("\n");
+    for (i = 1; i < n; ++i) {
+        for (j = 0; j < col; ++j) {
+            printf("%d ", v[LIN + j]);
+        }
+        k += i;
+        col--;
+        printf("\n");
+    }
 
 
 	return 0;
